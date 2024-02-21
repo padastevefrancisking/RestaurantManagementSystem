a0736648c7b39cb785a2a5b491685d6e6f5e93ac
GildedS <outerscience69@gmail.com>
2024-02-21 11:37:21 +0800

    Add project files.

---
+++ RestuarantGUI/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace RestuarantGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.Chicken = new System.Windows.Forms.RadioButton();
+            this.Rice = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.Chicken);
+            this.flowLayoutPanel1.Controls.Add(this.Rice);
+            this.flowLayoutPanel1.Controls.Add(this.radioButton1);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(159, 415);
+            this.flowLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // Chicken
+            // 
+            this.Chicken.Appearance = System.Windows.Forms.Appearance.Button;
+            this.Chicken.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Chicken.BackgroundImage = global::RestuarantGUI.Properties.Resources.Chimken;
+            this.Chicken.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Chicken.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Chicken.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Chicken.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.Chicken.Location = new System.Drawing.Point(3, 3);
+            this.Chicken.Name = "Chicken";
+            this.Chicken.Size = new System.Drawing.Size(139, 144);
+            this.Chicken.TabIndex = 0;
+            this.Chicken.UseVisualStyleBackColor = false;
+            this.Chicken.CheckedChanged += new System.EventHandler(this.Chicken_CheckedChanged);
+            // 
+            // Rice
+            // 
+            this.Rice.Appearance = System.Windows.Forms.Appearance.Button;
+            this.Rice.BackgroundImage = global::RestuarantGUI.Properties.Resources.Roice;
+            this.Rice.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Rice.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Rice.Location = new System.Drawing.Point(3, 153);
+            this.Rice.Name = "Rice";
+            this.Rice.Size = new System.Drawing.Size(149, 125);
+            this.Rice.TabIndex = 1;
+            this.Rice.UseVisualStyleBackColor = true;
+            this.Rice.CheckedChanged += new System.EventHandler(this.Rice_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton1.BackgroundImage = global::RestuarantGUI.Properties.Resources.Spaghetti;
+            this.radioButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.radioButton1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.radioButton1.Location = new System.Drawing.Point(3, 284);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(149, 111);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(262, 165);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(710, 468);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(348, 226);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(358, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(349, 228);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(990, 658);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.RadioButton Chicken;
+        private System.Windows.Forms.RadioButton Rice;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ RestuarantGUI/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestuarantGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Chicken_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Chicken.Checked)
+            {
+
+            }
+        }
+
+        private void Rice_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Rice.Checked)
+                MessageBox.Show("RICE");
+        }
+    }
+}
